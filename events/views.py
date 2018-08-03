56fcfacca89bf2cf06014e905c02aa47153bccfc
texasbones11 <willjones.ut@gmail.com>
2018-08-03 14:28:16 -0500

    update views

--- events/views.py
+++ events/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from models import Events
+from models import Events, Leaderboard, GolfCourse
 # Create your views here.
 def index(request):
         events = Events.objects.all()[:10]
@@ -12,7 +12,11 @@ def index(request):
 
 def leaderboard(request, id):
     event = Events.objects.get(id=id)
+    golfcourse = GolfCourse.objects.all()[0]
+    leaderboard = Leaderboard.objects.all()
     context = {
-            'event': event
+            'event': event,
+            'leaderboard': leaderboard,
+            'golfcourse': golfcourse
     }
     return render(request, 'events/leaderboard.html', context)
